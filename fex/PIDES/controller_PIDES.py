09550bbe61944c455a05d58f717a7d87683a8788
ghardwick16 <ghardwick16@gmail.com>
2023-10-12 10:02:34 -0700

    squeezed bc_true (controller ln 586)

--- fex/PIDES/controller_PIDES.py
+++ fex/PIDES/controller_PIDES.py
@@ -583,7 +583,7 @@ def get_reward(bs, actions, learnable_tree, tree_params, tree_optim):
         tree_optim = torch.optim.Adam(tree_params, lr=0.001)
         for _ in range(20):
             bd_pts = get_boundary(args.bdbs, dim)
-            bc_true = func.true_solution(bd_pts)
+            bc_true = torch.squeeze(func.true_solution(bd_pts))
             bd_nn = learnable_tree(bd_pts, bs_action)
             bd_error = torch.nn.functional.mse_loss(bc_true, bd_nn)
 
