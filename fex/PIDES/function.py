127b46c9f6c011edf9055fade96ef8f6456e9b1a
ghardwick16 <ghardwick16@gmail.com>
2023-10-13 10:30:12 -0700

    issue with shape of tensors in integrand

--- fex/PIDES/function.py
+++ fex/PIDES/function.py
@@ -31,7 +31,7 @@ def LHS_pde(func, tx): #changed to let this use the pair (learnable_tree, bs_act
     du = torch.autograd.grad(u, tx, grad_outputs=v, create_graph=True)[0]
     ut = du[:, 0].cuda()
     ux = du[:, 1].cuda()
-    integrand = torch.empty_like(u_expz).cuda()
+    integrand = torch.empty(tx.shape[0], num_traps).cuda()
     exp_z = torch.exp(z).cuda()
     for i in range(u_expz.shape[0]):
         integrand[i, :] = torch.mul((u_expz[i, :] - u[i] - x[i] * (exp_z - 1) * ux[i]), nu)
