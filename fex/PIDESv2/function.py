7208a4fab3aaa41659a8d474cfce7c015009ea1d
ghardwick16 <ghardwick16@gmail.com>
2023-10-27 11:37:46 -0400

    no more torchquad.  hard coded montecarlo integration.

--- fex/PIDESv2/function.py
+++ fex/PIDESv2/function.py
@@ -1,31 +1,43 @@
 import numpy as np
 import torch
 from torch import sin, cos, exp
-from torchquad import Trapezoid, set_up_backend
 import math
+
+
+def montecarlo_integration(function, domain, num_samples):
+    points = torch.empty((num_samples, len(domain)))
+    vol = 0
+    for i in range(len(domain)):
+        vol *= (domain[i][1] - domain[i][0])
+        points[:, i] = torch.rand(num_samples).cuda() * (domain[i][1] - domain[i][0]) + domain[i][0]
+    return vol / num_samples * torch.sum(function(points))
 
 
 def integrand(func, mu, sigma, lam, tx, z):
     ### We have two cases:  either we pass in the candidate function in the form
     ### (learnable_tree, bs_action) or the true function (for measuring performance)
-    t = tx[0].cuda()
-    x = tx[1:].cuda()
+    tx = tx.cuda()
+    z = z.cuda()
+
+    t = tx[:, 0]
+    x = tx[:, 1:]
+
     # u(t, x)
-    u = func(tx).cuda()
+    u = func(tx)
     # u(t, x + z)
     if len(z.shape) > 1:
-        tx_shift = torch.empty((z.shape[0], tx.shape[0]))
+        tx_shift = torch.empty((z.shape[0], tx.shape[0])).cuda()
         for i in range(z.shape[0]):
             tx_shift[i, 0] = t
             tx_shift[i, 1:] = x + z[i, :]
     else:
-        tx_shift = torch.empty_like(tx)
+        tx_shift = torch.empty_like(tx).cuda()
         tx_shift[0] = t
         tx_shift[1:] = x + z
     u_shift = func(tx_shift)
     # z dot grad u
     grad_u = torch.autograd.grad(u, tx, grad_outputs=torch.ones_like(u), create_graph=True)[0][1:].cuda()
-    dot_prod = torch.empty(z.shape[0])
+    dot_prod = torch.empty(z.shape[0]).cuda()
     if len(z.shape) > 1:
         for i in range(z.shape[0]):
             dot_prod[i] = torch.dot(z[i, :].float(), grad_u)
@@ -46,7 +58,8 @@ def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_a
     epsilon = 0
     # parameters for integration (done by Torchquad)
     num_ints = 3  # number of intervals to break up each dimension of x by
-    domain = torch.tensor([[0, 1]] * (tx.shape[1] - 1), dtype=torch.float32).cuda()  # integrating on [0,1] on each dim of x
+    domain = torch.tensor([[0, 1]] * (tx.shape[1] - 1),
+                          dtype=torch.float32).cuda()  # integrating on [0,1] on each dim of x
     domain.float()
     method = Trapezoid()
     set_up_backend("torch", data_type="float32")
@@ -79,14 +92,7 @@ def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_a
     int_fun = lambda var: integrand(u_func, mu, sigma, lam, point, var)
     for i in range(tx.shape[0]):
         point = tx[i, :]
-        print(point)
-        print(u_func(point))
-        temp = method.integrate(int_fun, dim=tx.shape[1] - 1,
-                                N=N, integration_domain=domain, backend='torch')
-        print(temp)
-        integral_dz[i] = method.integrate(int_fun,
-                                          dim=tx.shape[1] - 1,
-                                          N=N, integration_domain=domain, backend='torch')
+        integral_dz[i] = montecarlo_integration(int_fun, domain=domain, num_samples=50)
     return ut + epsilon / 2 * torch.dot(x, ux) + 1 / 2 * theta ** 2 * trace_hessian + integral_dz
 
 
