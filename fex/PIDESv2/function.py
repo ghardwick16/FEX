012e37d7041b9bfcee802e99d50c7ace27223f6e
ghardwick16 <ghardwick16@gmail.com>
2023-10-27 11:38:36 -0400

    removed a constant needed for torchquad but not for my own implementation of montecarlo

--- fex/PIDESv2/function.py
+++ fex/PIDESv2/function.py
@@ -87,7 +87,6 @@ def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_a
     trace_hessian = torch.sum(ddu[:, 1:], dim=1)
 
     # take the integral
-    N = num_ints ** (tx.shape[1] - 1)
     integral_dz = torch.empty(tx.shape[0])
     int_fun = lambda var: integrand(u_func, mu, sigma, lam, point, var)
     for i in range(tx.shape[0]):
