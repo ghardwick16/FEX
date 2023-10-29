1dd9caedf8aed028d05d22a64172289317e68a26
ghardwick16 <ghardwick16@gmail.com>
2023-10-29 14:29:53 -0400

    fixed true solutions and RHS PDE

--- fex/PIDESv2/function.py
+++ fex/PIDESv2/function.py
@@ -29,7 +29,7 @@ def integrand(func, u, du, mu, sigma, lam, tx, z):
     # nu
     # nu = lam/torch.sqrt(2*torch.Tensor([math.pi])*sigma)*torch.exp(-.5*((z-mu)/sigma)**2)
     # print(nu)
-    return u_shift - u.expand(u_shift.shape[0], u.shape[0]) - dot_prod
+    return (u_shift - u.expand(u_shift.shape[0], u.shape[0]) - dot_prod)*lam
 
 
 def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_action) for computation directly
