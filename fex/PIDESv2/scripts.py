14739c7e71fb0c8dc9dd36d240f1e5abb423c79f
ghardwick16 <ghardwick16@gmail.com>
2023-11-01 11:33:28 -0400

    fixed dimension error in scripts.py, got rid of print statements

--- fex/PIDESv2/scripts.py
+++ fex/PIDESv2/scripts.py
@@ -6,7 +6,7 @@ gpus = [1,7,8,9,2,3,4,5,6]*5
 idx = 0
 
 for i in range(5):
-    for dim in [2]:
+    for dim in [3]:
         gpu = gpus[idx]
         idx += 1
         os.system('python controller_PIDESv2.py --epoch 200 --bs 10 --greedy 0.1 --gpu '+str(gpu)+' --ckpt t_range_0_1_2ksearch_int20k_bd4kDim'+str(dim)+' --tree depth2_sub --random_step 3 --lr 0.002 --dim '+str(dim)+' --base 1000 --left 0 --right 1 --domainbs 1000 --bdbs 1000')
