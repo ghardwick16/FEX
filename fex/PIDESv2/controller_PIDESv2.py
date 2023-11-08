9f316de317b6fedd279520b1508dcaf3f6908557
ghardwick16 <ghardwick16@gmail.com>
2023-11-08 17:05:01 -0500

    trying stuff

--- fex/PIDESv2/controller_PIDESv2.py
+++ fex/PIDESv2/controller_PIDESv2.py
@@ -582,7 +582,7 @@ def get_reward(bs, actions, learnable_tree, tree_params, tree_optim, lam):
         # regression_error = torch.nn.functional.mse_loss(learnable_tree(x, bs_action), func.true_solution(x))
 
         reset_params(tree_params)
-        tree_optim = torch.optim.Adam(tree_params, lr=0.001)
+        tree_optim = torch.optim.Adam(tree_params, lr=0.1)
         print(tree_params)
         for _ in range(20):
             bd_pts = get_boundary(args.bdbs, dim)
