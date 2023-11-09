c6e43c3882807d03ba89bdfca5981b2c4af99377
ghardwick16 <ghardwick16@gmail.com>
2023-11-09 11:36:16 -0500

    the whole issue appears to be caused by my use of torch.sum in only True_solution!!!

--- fex/PIDES3.9/controller_PIDESv2.py
+++ fex/PIDES3.9/controller_PIDESv2.py
@@ -608,7 +608,7 @@ def get_reward(bs, actions, learnable_tree, tree_params, tree_optim):
             tree_optim.zero_grad()
 
             bd_pts = get_boundary(args.bdbs, dim)
-            bc_true = func.true_solution(bd_pts)
+            bc_true = func.true_solution(bd_pts).unsqueeze(1)
             bd_nn = learnable_tree(bd_pts, bs_action)
             bd_error = torch.nn.functional.mse_loss(bc_true, bd_nn)
             function_error = torch.nn.functional.mse_loss(func.LHS_pde(lhs_func, x), func.RHS_pde(x))
