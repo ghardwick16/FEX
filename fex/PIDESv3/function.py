f22bb7b1af6300ef501954b76039d26b4c2b1e5e
ghardwick16 <ghardwick16@gmail.com>
2023-11-09 10:43:30 -0500

    made v3 solve 3.9 in 1 d just to see if that magically works

--- fex/PIDESv3/function.py
+++ fex/PIDESv3/function.py
@@ -18,12 +18,11 @@ def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_a
 
     nu = lam / torch.sqrt(2 * torch.Tensor([math.pi]) * sigma).cuda() * torch.exp(-.5 * ((z - mu) / sigma).cuda() ** 2)
     tx_expz = torch.stack((t.repeat(z.shape[0], 1).T, torch.outer(x, torch.exp(z).cuda())), dim=2)
-    tx_large = tx.unsqueeze(1).repeat(1, z.shape[0], 1).cuda()
+    tx_shift = tx.unsqueeze(1).repeat(1, z.shape[0], 1).cuda()
     z_large = z.unsqueeze(0).repeat(tx.shape[0], 1, 1).cuda()
-    print(tx_large.shape)
+    print(tx_shift.shape)
     print(z_large.shape)
-    input = torch.cat((torch.unsqueeze(tx_large[..., 0], 2), (tx_large[..., 1:] + z_large)), dim=-1)
-    print(input.shape)
+    tx_shift[..., 1:] += z_large
 
     ### We have two cases:  either we pass in the condidate function in the form
     ### (learnable_tree, bs_action) or the true function (for measuring performance)
