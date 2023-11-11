51ee531d2d5be72022719e09952075138ab0a6e0
ghardwick16 <ghardwick16@gmail.com>
2023-11-11 12:32:36 -0500

    added jumps to 3.2

--- fex/PIDES3.2/function.py
+++ fex/PIDES3.2/function.py
@@ -3,11 +3,33 @@ import torch
 from torch import sin, cos, exp
 import math
 
-def LHS_pde(func, tx): #changed to let this use the pair (learnable_tree, bs_action) for computation directly
+
+def apply_jumps(tx, u, lam, mu, sigma, domain):
+    # sample poisson and norma dists for jump times and jumps
+    jump_t_dist = torch.distributions.exponential.Exponential(lam)
+    jump_dist = torch.distributions.normal.Normal(mu, sigma)
+    jump_times = [0]
+    while jump_times[-1] < domain[1]:
+        jump_times.append(sum(jump_times) + jump_t_dist.sample())
+    times = jump_times[1:-1]
+    jumps = jump_dist.sample([len(jump_times) - 2])
+    if len(times) == 0:
+        return u
+    else:
+        i = 0
+        for time in times:
+            print(i)
+            idxs = torch.where(tx[..., 0] > time)
+            u[idxs] += jumps[i]
+            i += 1
+        return u
+
+
+def LHS_pde(func, tx):  # changed to let this use the pair (learnable_tree, bs_action) for computation directly
     mu = .4
     sigma = .25
     lam = .3
-    num_traps = 5 #traps super low to keep code faster
+    num_traps = 5  # traps super low to keep code faster
     z = torch.linspace(0, 1, num_traps).cuda()
     t = torch.squeeze(tx[..., 0]).cuda()
     x = torch.squeeze(tx[..., 1:]).cuda()
@@ -23,7 +45,8 @@ def LHS_pde(func, tx): #changed to let this use the pair (learnable_tree, bs_act
     else:
         u_func = lambda y: func(y)
 
-    u = torch.squeeze(u_func(tx))
+    u = torch.squeeze(apply_jumps(tx, u_func(tx), lam, mu, sigma, domain=[0, 1]))
+
     u_expz = torch.squeeze(u_func(tx_expz))
     v = torch.ones(u.shape).cuda()
     du = torch.autograd.grad(u, tx, grad_outputs=v, create_graph=True)[0]
@@ -31,32 +54,33 @@ def LHS_pde(func, tx): #changed to let this use the pair (learnable_tree, bs_act
     ux = du[:, 1].cuda()
     exp_z = torch.exp(z).cuda()
     integrand = (u_expz - u.repeat(z.shape[0], 1).T - x.repeat(z.shape[0], 1).T * (
-                exp_z.repeat(tx.shape[0], 1) - 1) * ux.repeat(z.shape[0], 1).T) * nu.repeat(tx.shape[0], 1)
+            exp_z.repeat(tx.shape[0], 1) - 1) * ux.repeat(z.shape[0], 1).T) * nu.repeat(tx.shape[0], 1)
     integral_dz = torch.trapezoid(integrand, z, dim=1)
     return ut + integral_dz
+
 
 def RHS_pde(tx):
     bs = tx.size(0)
     return torch.zeros(bs, 1).cuda()
 
-def true_solution(tx): #for the most simple case, u(t,x) = x
+
+def true_solution(tx):  # for the most simple case, u(t,x) = x
     return tx[..., 1:]
 
 
-unary_functions = [lambda x: 0*x**2,
-                   lambda x: 1+0*x**2,
-                   lambda x: x+0*x**2,
-                   lambda x: x**2,
-                   lambda x: x**3,
-                   lambda x: x**4,
+unary_functions = [lambda x: 0 * x ** 2,
+                   lambda x: 1 + 0 * x ** 2,
+                   lambda x: x + 0 * x ** 2,
+                   lambda x: x ** 2,
+                   lambda x: x ** 3,
+                   lambda x: x ** 4,
                    torch.exp,
                    torch.sin,
-                   torch.cos,]
+                   torch.cos, ]
 
-binary_functions = [lambda x,y: x+y,
-                    lambda x,y: x*y,
-                    lambda x,y: x-y]
-
+binary_functions = [lambda x, y: x + y,
+                    lambda x, y: x * y,
+                    lambda x, y: x - y]
 
 unary_functions_str = ['({}*(0)+{})',
                        '({}*(1)+{})',
@@ -69,23 +93,22 @@ unary_functions_str = ['({}*(0)+{})',
                        # '({})**5',
                        '({}*exp({})+{})',
                        '({}*sin({})+{})',
-                       '({}*cos({})+{})',]
-                       # 'ref({})',
-                       # 'exp(-({})**2/2)']
+                       '({}*cos({})+{})', ]
+# 'ref({})',
+# 'exp(-({})**2/2)']
 
-unary_functions_str_leaf= ['(0)',
-                           '(1)',
-                           # '5',
-                           '({})',
-                           # '-{}',
-                           '(({})**2)',
-                           '(({})**3)',
-                           '(({})**4)',
-                           # '({})**5',
-                           '(exp({}))',
-                           '(sin({}))',
-                           '(cos({}))',]
-
+unary_functions_str_leaf = ['(0)',
+                            '(1)',
+                            # '5',
+                            '({})',
+                            # '-{}',
+                            '(({})**2)',
+                            '(({})**3)',
+                            '(({})**4)',
+                            # '({})**5',
+                            '(exp({}))',
+                            '(sin({}))',
+                            '(cos({}))', ]
 
 binary_functions_str = ['(({})+({}))',
                         '(({})*({}))',
