e14a17bd686783640913bdb3567ae611d6ec9e07
ghardwick16 <ghardwick16@gmail.com>
2023-11-18 12:55:20 -0800

    Created stochastic version of 3.2, called PIDES3.2v2

--- fex/PIDES3.2v2/function.py
+++ fex/PIDES3.2v2/function.py
@@ -78,11 +78,10 @@ def get_loss(func, true, x_t, jump_mat):
     u_exp_jumps = u(tx_exp_jumps)
     u_tx = u(torch.cat((t.unsqueeze(2), x_t.unsqueeze(2)), dim=2))
     u_expz = torch.squeeze(u(tx_expz))
-    print(u_expz.shape)
-    print(phi.unsqueeze(1).repeat(x_t.shape[0], 1, x_t.shape[1]).shape)
-    print(u_tx.shape)
     integral = torch.trapezoid(u_expz * phi.unsqueeze(1).repeat(x_t.shape[0], 1, x_t.shape[1]),
                                 dx=(domain[1] - domain[0]) / num_pts, dim=1)
+    print(integral.shape)
+    print(u_tx.shape)
     n2 = lam * (integral - u_tx)
     loss1 = torch.mean((u_exp_jumps[..., :-1] - dt * n2[..., :-1] - u_tx[..., 1:]) ** 2)
 
