43f191e1a0bac2471552e6cede7c03fee0c3a3d7
ghardwick16 <ghardwick16@gmail.com>
2023-11-18 12:39:16 -0800

    Created stochastic version of 3.2, called PIDES3.2v2

---
+++ fex/PIDES3.2v2/scripts.py
@@ -0,0 +1,13 @@
+import os
+import time
+import random
+
+gpus = [1,7,8,9,2,3,4,5,6]*5
+idx = 0
+
+for i in range(5):
+    for dim in [2]:
+        gpu = gpus[idx]
+        idx += 1
+        os.system('python controller_PIDES.py --epoch 200 --bs 10 --greedy 0.1 --gpu '+str(gpu)+' --ckpt t_range_0_1_2ksearch_int20k_bd4kDim'+str(dim)+' --tree depth2_sub --random_step 3 --lr 0.002 --dim '+str(dim)+' --base 1000 --left 0 --right 1 --domainbs 1000 --bdbs 1000')
+        time.sleep(500)
