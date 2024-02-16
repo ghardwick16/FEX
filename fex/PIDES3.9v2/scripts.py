a1e7cad49e8998ba4b9a9606e880d9272a9be61e
ghardwick16 <ghardwick16@gmail.com>
2024-02-16 18:26:10 -0500

    Cranked everything back up after successful testing

--- fex/PIDES3.9v2/scripts.py
+++ fex/PIDES3.9v2/scripts.py
@@ -10,6 +10,6 @@ for dim in [6, 11, 21, 51, 101]:
     idx += 1
     os.system('python controller_PIDESv2.py --epoch 200 --bs 10 --greedy 0.1 --gpu ' + str(
             gpu) + ' --ckpt t_range_0_1_2ksearch_int20k_bd4kDim' + str(
-            dim) + ' --tree depth2 --random_step 3 --lr 0.001 --dim ' + str(
+            dim) + ' --tree depth2_sub --random_step 3 --lr 0.001 --dim ' + str(
             dim) + ' --base 1000 --left 0 --right 1')
     time.sleep(500)
