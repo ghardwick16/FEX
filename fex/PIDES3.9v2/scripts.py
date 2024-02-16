db70b4280f3e8fd7171a7e2251949cca12c1ebe8
ghardwick16 <ghardwick16@gmail.com>
2024-02-16 12:33:17 -0500

    testing

--- fex/PIDES3.9v2/scripts.py
+++ fex/PIDES3.9v2/scripts.py
@@ -5,12 +5,11 @@ import random
 gpus = [1,7,8,9,2,3,4,5,6]*5
 idx = 0
 
-for i in range(5):
-    for dim in [51, 101]:
-        gpu = gpus[idx]
-        idx += 1
-        os.system('python controller_PIDESv2.py --epoch 200 --bs 10 --greedy 0.1 --gpu ' + str(
+for dim in [3]:
+    gpu = gpus[idx]
+    idx += 1
+    os.system('python controller_PIDESv2.py --epoch 200 --bs 10 --greedy 0.1 --gpu ' + str(
             gpu) + ' --ckpt t_range_0_1_2ksearch_int20k_bd4kDim' + str(
             dim) + ' --tree depth2_sub --random_step 3 --lr 0.002 --dim ' + str(
             dim) + ' --base 1000 --left 0 --right 1')
-        time.sleep(500)
+    time.sleep(500)
