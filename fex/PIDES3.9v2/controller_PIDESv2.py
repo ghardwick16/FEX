0409923a2a061755a5aa90f0b1ba539a28a6d81c
ghardwick16 <ghardwick16@gmail.com>
2024-02-19 14:16:44 -0500

    printing tree params and formula to see structure

--- fex/PIDES3.9v2/controller_PIDESv2.py
+++ fex/PIDES3.9v2/controller_PIDESv2.py
@@ -675,7 +675,7 @@ def get_reward(bs, actions, learnable_tree, tree_params, tree_optim):
         count = 0
         leaves_cnt = 0
         formulas.append(formula)
-        print(tree_params)
+        print(tree_params[-4:])
 
     return regression_errors, formulas
 
