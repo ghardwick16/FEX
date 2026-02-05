f4a0f9e629499f03e1c6c48bc1b34e7f2b3a964f
ghardwick16 <133396746+ghardwick16@users.noreply.github.com>
2026-02-05 10:59:55 -0700

    Delete fex/PIDES3.5 directory

--- fex/PIDES3.5/scripts.py
+++
@@ -1,9 +0,0 @@
-import os
-
-gpus = [1]*5
-idx = 0
-dim = 2
-for _ in range(5):
-    gpu = gpus[idx]
-    idx += 1
-    os.system('screen python controller_PIDESv3.py --epoch 50 --bs 10 --greedy 0.1 --gpu '+str(gpu)+' --ckpt t_range_0_1_2ksearch_int20k_bd4kDim'+str(dim)+' --tree depth2_sub --random_step 3 --lr 0.001 --dim '+str(dim)+' --base 1000 --left 0 --right 1')
